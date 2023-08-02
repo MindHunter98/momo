8ebce21cafe09888aabf819f25d341acaf99ab43
MindHunter <121933544+MindHunter98@users.noreply.github.com>
2023-08-02 12:39:08 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=9279713&pid=19c1b467ec4d34091a0ecf58554513a4&tid=2c07344e4f995870e5fd974383629481",
+    "https://www.maimemo.com/share/page?uid=9279713&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=fed8e675a2523bfc06c46c01f087f5aa",
     //"",
     //"",
   ]
